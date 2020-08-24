3faaceb0d5e86306e6b52d278af6b7c68380d8f6
olgab <olgabresheva7@gmail.com>
2020-08-24 03:52:19 -0700

    Updates

---
+++ precache-manifest.63c84d5e4cc2d1013b24b9873d6c0d86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d853614097d21bf05308e798fc19340",
+    "url": "/todo-redux-api/index.html"
+  },
+  {
+    "revision": "28fbdb264e3d95efdae6",
+    "url": "/todo-redux-api/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a0cf8ee62344aa40b4dc",
+    "url": "/todo-redux-api/static/css/main.572daa22.chunk.css"
+  },
+  {
+    "revision": "28fbdb264e3d95efdae6",
+    "url": "/todo-redux-api/static/js/2.94f790a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-redux-api/static/js/2.94f790a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0cf8ee62344aa40b4dc",
+    "url": "/todo-redux-api/static/js/main.bfbcb325.chunk.js"
+  },
+  {
+    "revision": "db6fe14c6782531634ba",
+    "url": "/todo-redux-api/static/js/runtime-main.c366c178.js"
+  }
+]);
